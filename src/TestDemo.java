1955350682bfb259b6739706262fe87244b482c1
ZShuangShuang <1434278632@qq.com>
2020-11-04 15:11:25 +0800

    Initial commit

---
+++ src/TestDemo.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 14342
+ * Date: 2020-11-04
+ * Time: 15:05
+ **/
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("I am a student");
+    }
+}
